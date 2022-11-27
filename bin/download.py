73b7dc26181fc5447dbf673c9b20043624a67ac7
napo <napo@fbk.eu>
2022-11-27 21:35:26 +0100

    download

---
+++ bin/download.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+import geopandas as gpd
+from owslib.wfs import WebFeatureService
+import os
+wfs_url="http://sit.comune.bolzano.it/geoserver/wfs?service=WFS"
+wfs = WebFeatureService(url=wfs_url, version='2.0.0')
+layers = list(wfs.contents)
+incidenti = []
+for layer in layers:
+    if layer.find('ciden') > -1:
+        incidenti.append(layer)
+for incidente in incidenti:
+    response = wfs.getfeature(typename=incidente)
+    data = gpd.read_file(response)
+    data.crs="epsg:25832"
+    data = data.to_crs(epsg=4326)
+    data['DTINCID']=data['DTINCID'].astype(str)
+    data.DTINCID =data.DTINCID.apply(lambda x: x.replace(' 00:00:00+00:00',""))
+    data['lon'] = data.geometry.x
+    data['lat'] = data.geometry.y
+    name = incidente.replace("Cartografia:view_","")
+    data.to_file(".." + os.sep + "data" + os.sep + name + ".geojson",driver="GeoJSON")
+    del data['geometry']
+    data.to_csv(".." + os.sep + "data" + os.sep + name + ".csv",index=False)
+
